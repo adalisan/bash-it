881129036265a3ef27bd24659641eb688c3b1488
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-09-21 03:42:36 +0200

    configuring bash-it for linux machines

---
+++ aliases/available/jenkins.aliases.bash
@@ -0,0 +1,7 @@
+cite 'jenkins aliases'
+about-alias 'general aliases to manipulate jenkins installation'
+
+jenkins_start='/etc/init.d/jenkins start'
+jenkins_stop='/etc/init.d/jenkins stop'
+jenkins_restart='/etc/init.d/restart'
+jenkins_configiration='vi /etc/defaults/jenkins'
